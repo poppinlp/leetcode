0d062bbbc8f6c572be70428e64d9233557075eb8
PoppinL <poppinlp@gmail.com>
2020-02-23 16:13:46 +0800

    add 1347

--- code/1347.js
+++ code/1347.js
@@ -1,5 +1,22 @@
 const minSteps = (s, t) => {
   const BASE = 97;
+  const count1 = new Int32Array(26);
+  const count2 = new Int32Array(26);
+  for (let i = 0; i < s.length; ++i) {
+    ++count1[s.charCodeAt(i) - BASE];
+  }
+  for (let i = 0; i < t.length; ++i) {
+    ++count2[t.charCodeAt(i) - BASE];
+  }
+  let step = 0;
+  for (let i = 0; i < 26; ++i) {
+    step += count1[i] > count2[i] ? count1[i] - count2[i] : 0;
+  }
+  return step;
+};
+
+const minSteps = (s, t) => {
+  const BASE = 97;
   const count = new Int32Array(26);
   for (let i = 0; i < s.length; ++i) {
     ++count[s.charCodeAt(i) - BASE];
@@ -11,5 +28,3 @@ const minSteps = (s, t) => {
   }
   return step;
 };
-
-console.log(minSteps('bab', 'aba'))
